465a16eac8eb58c2fbd4ae34ff8395f6bed59a6e
vetorL <145482274+vetorL@users.noreply.github.com>
2024-01-10 18:20:59 -0300

    Adiciona informações dos membros do grupo

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ep-aed-1
+
 EP de ACH2023 - Algoritmos e Estruturas de Dados I (2023)
+
+# Membros do grupo:
+
+==========
+Nome: Karen Miyuki Massuda
+Número USP: 14585868
+Turma: 04
+==========
+Nome: Vivian Ayumi Miamura
+Número USP: 14835140
+Turma: 94
+==========
+Nome: Vítor Benvenisti Laguna Navarenho
+Número USP: 14749425
+Turma: 94
+==========
--- ep.c
+++ ep.c
@@ -1,3 +1,18 @@
+// Membros do grupo:
+// ==========
+// Nome: Karen Miyuki Massuda 
+// Número USP: 14585868
+// Turma: 04
+// ========== 
+// Nome: Vivian Ayumi Miamura 
+// Número USP: 14835140
+// Turma: 94
+// ========== 
+// Nome: Vítor Benvenisti Laguna Navarenho 
+// Número USP: 14749425
+// Turma: 94
+// ==========  
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
