73c70be5fbce72b1f459d88a39c71dfce0910883
vetorL <145482274+vetorL@users.noreply.github.com>
2024-01-15 02:31:22 -0300

    Formata codigo

--- arvore.h
+++ arvore.h
@@ -1,10 +1,20 @@
 #include "arvore.c"
 
-void insere_arvore(char *palavra);
-bool busca_arvore(char *palavra);
-typedef struct {
+typedef struct _no_arvore_
+{
 
-	No * raiz;
+    Elemento valor;
+    struct _no_arvore_ *esq;
+    struct _no_arvore_ *dir;
+
+} No;
+
+typedef struct
+{
+
+    No *raiz;
 
 } Arvore;
 
+void insere_arvore(char *palavra);
+bool busca_arvore(char *palavra);
