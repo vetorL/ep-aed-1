fa62261408eafd5164f1e81b9af419c230ff55ac
vetorL <145482274+vetorL@users.noreply.github.com>
2024-01-15 04:29:47 -0300

    Remove codigo comentado

--- arvore.c
+++ arvore.c
@@ -3,15 +3,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <string.h>
-// #include <math.h>
-
-// bool __debug__ = false;
-// void debug_on() { __debug__ = true; }
-// void debug_off() { __debug__ = false; }
-
-// #define ROWS 20
-// #define COLS 80
 
 LinhaAr *cria_linha_ar(char *texto_linha, int num_linha)
 {
@@ -107,84 +98,3 @@ NoAr *busca_arvore(Arvore *arvore, char *palavra)
 
     return busca_rec(arvore->raiz, palavra);
 }
-
-// int display_rec(char **buffer, NoAr *no, int level, double h_position)
-// {
-
-//     char *ptr;
-//     int i, col, a, b;
-//     double offset;
-
-//     if (no)
-//     {
-//         col = (int)(h_position * COLS);
-//         offset = 1.0 / pow(2, level + 2);
-
-//         ptr = buffer[1 + level * 3] + col;
-//         sprintf(ptr, "%s", no->valor->palavra);
-//         *(ptr + strlen(ptr)) = ' ';
-
-//         if (no->esq || no->dir)
-//             *(buffer[2 + level * 3] + col + 1) = '|';
-
-//         if (no->esq)
-//         {
-
-//             i = (int)((h_position - offset) * COLS);
-//             *(buffer[3 + level * 3] + 1 + i) = '|';
-//             i++;
-//             for (; i <= col; i++)
-//                 *(buffer[3 + level * 3] + 1 + i) = '-';
-//         }
-
-//         if (no->dir)
-//         {
-
-//             for (i = col; i < (int)((h_position + offset) * COLS); i++)
-//                 *(buffer[3 + level * 3] + 1 + i) = '-';
-//             *(buffer[3 + level * 3] + 1 + i) = '|';
-//         }
-
-//         a = display_rec(buffer, no->esq, level + 1, h_position - offset);
-//         b = display_rec(buffer, no->dir, level + 1, h_position + offset);
-
-//         if (a > b)
-//             return a;
-//         return b;
-//     }
-
-//     return level;
-// }
-
-// void display_no(NoAr *no)
-// {
-
-//     int i, j, r;
-
-//     char **buffer = (char **)malloc(ROWS * sizeof(char *));
-
-//     for (i = 0; i < ROWS; i++)
-//     {
-
-//         buffer[i] = (char *)malloc((COLS + 1) * sizeof(char));
-
-//         for (j = 0; j < COLS; j++)
-//             buffer[i][j] = ' ';
-//         buffer[i][j] = '\0';
-//     }
-
-//     r = display_rec(buffer, no, 0, 0.5);
-
-//     if (__debug__)
-//         getchar();
-
-//     for (i = 0; i < 3 * r; i++)
-//         printf("%s\n", buffer[i]);
-//     printf("-----------------------------------------------------------------------------------------------\n");
-// }
-
-// void display(Arvore *arvore)
-// {
-
-//     display_no(arvore->raiz);
-// }
