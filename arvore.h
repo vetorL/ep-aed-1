7bc1094d936a92001f0bd276ed04b6bf19cee90d
vetorL <145482274+vetorL@users.noreply.github.com>
2024-01-15 02:31:03 -0300

    Define struct Arvore

--- arvore.h
+++ arvore.h
@@ -1,13 +1,10 @@
 #include "arvore.c"
 
-typedef struct _no_arvore_
-{
-
-    Elemento valor;
-    struct _no_arvore_ *esq;
-    struct _no_arvore_ *dir;
-
-} No;
-
 void insere_arvore(char *palavra);
 bool busca_arvore(char *palavra);
+typedef struct {
+
+	No * raiz;
+
+} Arvore;
+
