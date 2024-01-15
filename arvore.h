fd8f26be4895d2f66500570f9d2ee862a78d4070
vetorL <145482274+vetorL@users.noreply.github.com>
2024-01-15 02:30:41 -0300

    Define struct No

--- arvore.h
+++ arvore.h
@@ -1,4 +1,13 @@
 #include "arvore.c"
 
+typedef struct _no_arvore_
+{
+
+    Elemento valor;
+    struct _no_arvore_ *esq;
+    struct _no_arvore_ *dir;
+
+} No;
+
 void insere_arvore(char *palavra);
 bool busca_arvore(char *palavra);
